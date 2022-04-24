47f1b2b75fb8653d8917518a50c0a4713033e91b
Harry Lazaridis <harrylaz@kth.se>
2022-04-24 23:56:54 +0200

    hello

--- src/presenters/BodyPresenter.js
+++ src/presenters/BodyPresenter.js
@@ -17,65 +17,38 @@ function BodyPresenter() {
 
             let blobs = api_client.containerClient.listBlobsFlat();
 
+            console.log(blobs);
+
             let arrayForBlobs = [];
-
+            let index = 1;
             for await (const blob of blobs) {
-                let index = 1;
-
-                arrayForBlobs.push(blob.name); 
-                arrayForBlobs.push(
-                    blob.properties.createdOn.getDate() + "/" + 
+                let t = {
+                    images: blob.name,
+                    datesAndTime: blob.properties.createdOn.getDate() + "/" + 
                     (blob.properties.createdOn.getMonth()+1) + "-" + 
                     blob.properties.createdOn.getFullYear() + " " + 
                     blob.properties.createdOn.getHours() + ":" + 
                     blob.properties.createdOn.getMinutes() + ":" + 
-                    blob.properties.createdOn.getSeconds()
-                )
-                arrayForBlobs.push(index)
+                    blob.properties.createdOn.getSeconds(),
+                    blob,
+                } 
+                
+                arrayForBlobs.push(t)
                 index++;
             }
 
-            /**
-             * Divides array into smaller arrays.
-             * 
-             * @param {array to divide} array 
-             * @param {size of each division} K 
-             * @param {array length} N 
-             * @returns the divided array
-             */
-            function divideArray(array, K, N) {
-                let ans = [];
-                let temp = [];
-                for (let i = 0; i < N; i++) {
-                    temp.push(array[i]);
-                    if (((i + 1) % K) === 0) {
-                        ans.push(temp);
-                        temp = [];
-                    }
-                }
-                if (temp.length !== 0) {
-                    let a = temp.length;
-                    while (a !== K) {
-                        temp.push(0);
-                        a++;
-                    }
-                    ans.push(temp);
-                }
-                return ans;
-            }
-
-            let splittedArray = divideArray(arrayForBlobs, 3, arrayForBlobs.length).reverse();
-
-            setBlobs(splittedArray);
+            setBlobs(arrayForBlobs);
 
         }
         return blobStorage;
-    }, [blobs]) 
+    }, []) 
 
-    // console.log(blobs)
+
     
-    const redirect = (index) => {
-        return navigate("/summary/" + index);
+    const redirect = (index, blob) => {
+        return navigate("/summary/" + index, {
+            blob
+        });
     }
 
     const downloadImage = () => {
@@ -83,20 +56,45 @@ function BodyPresenter() {
         saveAs(blobs[0][1], "hello world.txt");
     }
 
+    if (!blobs) {
+        return (<h1>Loading...</h1>)
+    }
+
+    console.log(blobs)
+
     return (
         <div className="bodyPresenter">
-                {blobs.map((blob, i) => (
+
+                {blobs.map((blob, i) => {
+
+                    console.log(blob[i])
+
+                    return (
+                        <div key={i} className="elementBox">
+                            <BodyView
+                                images={blob[i].images}
+                                datesAndTime={blob[i].datesAndTime}  
+                                index ={i}
+                                redirect={redirect}
+                                key={i}    
+                            />
+                        </div>
+
+                    )
+                })}
+                
+                {/* {blobs.map((blob, i) => (
                     <div key={i} className="elementBox">
                         <BodyView
-                            images={blob[0]}
-                            datesAndTime={blob[1]}  
-                            index ={blob[2]}
+                            images={blob[i].images}
+                            datesAndTime={blob[i].datesAndTime}  
+                            index ={i}
                             redirect={redirect}
                             key={i}    
                         />
                     </div>
-                ))}   
-                <button onClick={downloadImage}>DOWNLOAD TEST</button>
+                ))}    */}
+                {/* <button onClick={downloadImage}>DOWNLOAD TEST</button> */}
         </div >
     )
 }
