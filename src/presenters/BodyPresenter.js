84412360c0fb9eb325fa8cc470a291773bd68d86
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-04-25 16:32:52 +0200

    updates

--- src/presenters/BodyPresenter.js
+++ src/presenters/BodyPresenter.js
@@ -4,46 +4,21 @@ import BodyView from '../views/BodyView';
 import { useNavigate } from 'react-router-dom';
 import '../views/css/Body.css'
 import { saveAs } from 'file-saver'
+import BlobRetriever from '../services/BlobRetriever'
 
 function BodyPresenter() {
 
+    // Navigate the user around the website
+    const navigate = useNavigate();
     const [blobs, setBlobs] = useState([]);
 
-    //Navigate the user around the website
-    const navigate = useNavigate();
-
     useEffect(() => {
-        async function blobStorage() {
 
-            let blobs = api_client.containerClient.listBlobsFlat();
+        BlobRetriever.blobData().then(function(result) {
+            setBlobs(result)
+        })
 
-            console.log(blobs);
-
-            let arrayForBlobs = [];
-            let index = 1;
-            for await (const blob of blobs) {
-                let t = {
-                    images: blob.name,
-                    datesAndTime: blob.properties.createdOn.getDate() + "/" + 
-                    (blob.properties.createdOn.getMonth()+1) + "-" + 
-                    blob.properties.createdOn.getFullYear() + " " + 
-                    blob.properties.createdOn.getHours() + ":" + 
-                    blob.properties.createdOn.getMinutes() + ":" + 
-                    blob.properties.createdOn.getSeconds(),
-                    blob,
-                } 
-                
-                arrayForBlobs.push(t)
-                index++;
-            }
-
-            setBlobs(arrayForBlobs);
-
-        }
-        return blobStorage;
-    }, []) 
-
-
+    }, [blobs]) 
     
     const redirect = (index, blob) => {
         return navigate("/summary/" + index, {
@@ -51,49 +26,26 @@ function BodyPresenter() {
         });
     }
 
-    const downloadImage = () => {
-        saveAs(api_client.get_image_url(blobs[0][0]), blobs[0][0])
-        saveAs(blobs[0][1], "hello world.txt");
-    }
-
-    if (!blobs) {
-        return (<h1>Loading...</h1>)
-    }
-
-    console.log(blobs)
+    // console.log(blobs)
+    // const downloadImage = () => {
+    //     saveAs(api_client.get_image_url(blobs[0][0]), blobs[0][0])
+    //     saveAs(blobs[0][1], "hello world.txt");
+    // }
 
     return (
         <div className="bodyPresenter">
-
-                {blobs.map((blob, i) => {
-
-                    console.log(blob[i])
-
-                    return (
-                        <div key={i} className="elementBox">
-                            <BodyView
-                                images={blob[i].images}
-                                datesAndTime={blob[i].datesAndTime}  
-                                index ={i}
-                                redirect={redirect}
-                                key={i}    
-                            />
-                        </div>
-
-                    )
-                })}
-                
-                {/* {blobs.map((blob, i) => (
+                {blobs.map((blob, i) => (
                     <div key={i} className="elementBox">
                         <BodyView
-                            images={blob[i].images}
-                            datesAndTime={blob[i].datesAndTime}  
-                            index ={i}
+                            images={blob.images}
+                            datesAndTime={blob.datesAndTime}  
+                            index = {i}
                             redirect={redirect}
-                            key={i}    
+                            key={i}   
+                            blob={blob}    
                         />
                     </div>
-                ))}    */}
+                ))}   
                 {/* <button onClick={downloadImage}>DOWNLOAD TEST</button> */}
         </div >
     )
--- src/views/BodyView.js
+++ src/views/BodyView.js
@@ -4,9 +4,11 @@ import * as api_client from '../services/api_client'
 
 function BodyView({ images, datesAndTime, index, redirect, blob }) {
 
+    // console.log(blob)
+    
     return (
         <div className="bodyView">
-            <div className="blobs" onClick={() => redirect(images, blob)}>
+            <div className="blobs" onClick={() => redirect(index, blob)}>
                     <img className="image" src={api_client.get_image_url(images)} alt=""/>
                     <div className="dateAndTime">{datesAndTime}</div>
             </div>
